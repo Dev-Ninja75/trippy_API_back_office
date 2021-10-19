7a047e9e46ddbea4c0d626be2fd0c82a11c3a6f4
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-10-19 12:02:24 +0200

    first commit

--- index.js
+++ index.js
@@ -7,27 +7,12 @@ const hotelsRoutes = require("./router/hotels");
 
 // hotels array storrage
 const hotels = ["Hilton", "Ritz", "Trianon Palace"];
-
-// hotels route
-app.get("/hotels", (req, res) => {
-  res.json(hotels);
-});
-
-// hotels id - array index
-app.get("/hotels/:id", (req, res) => {
-  const index = req.params.id;
-  res.json(hotels[index]);
-});
-
-// Add a new hotel
-app.post("/hotels", (req, res) => {
-  const hotel = req.body.name;
-  hotels.push(hotel);
-  res.json({ success: true });
-});
+console.log(hotels);
 
 // hotels router
-app.use("/hotels", hotelsRoutes),
-  server.listen(port, () => {
-    console.log(`NodeJS server started ${port}`);
-  });
+app.use("/hotels", hotelsRoutes);
+
+// Setup Server
+server.listen(port, () => {
+  console.log(`NodeJS server started ${port}`);
+});
