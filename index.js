3868c17eb39c77f0fca00e9a2276c094c6ccfa0d
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-10-19 17:31:18 +0200

    Setup router

--- index.js
+++ index.js
@@ -7,7 +7,31 @@ const hotelsRoutes = require("./router/hotels");
 
 // hotels array storrage
 const hotels = ["Hilton", "Ritz", "Trianon Palace"];
-console.log(hotels);
+
+// hotels route
+app.get("/hotels", (req, res) => {
+  res.json(hotels);
+});
+
+// hotels id => array index
+app.get("/hotels/:id", (req, res) => {
+  const index = req.params.id;
+  res.json(hotels[index]);
+});
+
+// Add a new hotel
+app.post("/hotels", (req, res) => {
+  const hotel = req.body.name;
+  hotels.push(hotel);
+  res.json({ success: true });
+});
+
+// Hotel name update
+app.put("/hotels/:id?name=newname", (req, res) => {
+  // /hotels/id:mqjslkhqsdh ?name=Ibis
+  console.log(req.query);
+  console.log(req.params);
+});
 
 // hotels router
 app.use("/hotels", hotelsRoutes);
